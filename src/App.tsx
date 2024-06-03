5d3fd19eac367c8bfcf09ec91fb3c01363468963
Sui Zer <suizer@nus.edu.sg>
2024-06-03 20:13:37 +0800

    Added esri basemap, still unauthorised

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import About from './components/About'
 import Home from './components/Home'
-import Map from './components/Map'
+import MapPort from './components/Map'
 
 // import Map from './pages/Map'
 
@@ -18,7 +18,7 @@ function App() {
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/map" element={<Map />} />
+          <Route path="/map" element={<MapPort />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,49 +1,35 @@
-import WebMap from '@arcgis/core/WebMap'
-import IdentityManager from '@arcgis/core/identity/IdentityManager'
-import OAuthInfo from '@arcgis/core/identity/OAuthInfo'
+import Basemap from '@arcgis/core/Basemap.js'
+import Map from '@arcgis/core/Map'
 import MapView from '@arcgis/core/views/MapView'
 import { useEffect } from 'react'
 
-const Map = () => {
+const MapPort = () => {
   useEffect(() => {
-    const clientId = import.meta.env.CLIENT_ID
-    // const clientSecret = import.meta.env.CLIENT_SECRET
-
-    const info = new OAuthInfo({
-      appId: clientId,
-      portalUrl: 'https://www.arcgis.com',
-      popup: false
+    const basemap = new Basemap({
+      style: {
+        id: 'arcgis/outdoor'
+      }
     })
 
-    IdentityManager.registerOAuthInfos([info])
+    const map = new Map({
+      basemap: basemap
+    })
 
-    IdentityManager.getCredential(info.portalUrl + '/sharing')
-      .then(() => {
-        const webmap = new WebMap({
-          portalItem: {
-            id: 'your-webmap-id' // Replace with your actual WebMap ID
-          }
-        })
+    const viewDiv = document.getElementById('viewDiv') as HTMLDivElement
 
-        const viewDiv = document.getElementById('viewDiv') as HTMLDivElement
+    const view = new MapView({
+      container: viewDiv,
+      map: map
+    })
 
-        const view = new MapView({
-          container: viewDiv,
-          map: webmap
-        })
-
-        return () => {
-          if (view) {
-            view.container = null
-          }
-        }
-      })
-      .catch((error) => {
-        console.error('Error getting credentials:', error)
-      })
+    return () => {
+      if (view) {
+        view.container = null
+      }
+    }
   }, [])
 
   return <div id="viewDiv" style={{ height: '100vh', width: '100vw' }}></div>
 }
 
-export default Map
+export default MapPort
