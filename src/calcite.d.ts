f3850b4858715f40140a88d2d04b452466e6597b
Sui Zer <suizer@nus.edu.sg>
2024-06-06 10:32:53 +0800

    Added calcite.d.ts to ignore the indirect jsx import error

---
+++ src/calcite.d.ts
@@ -0,0 +1,8 @@
+// This is because I placed reference in HTML Head
+declare namespace JSX {
+  interface IntrinsicElements {
+    'calcite-label': any
+    'calcite-combobox': any
+    'calcite-combobox-item': any
+  }
+}
