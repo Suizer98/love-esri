11f7ac2e43759291d12ac60042cb0a48d7362a6a
Sui Zer <teysuizer1998@gmail.com>
2024-06-08 18:39:52 +0800

    Made sure map widgets content not unseenable

--- src/components/features/Map/MapSearchWidget.tsx
+++ src/components/features/Map/MapSearchWidget.tsx
@@ -3,7 +3,7 @@ import Search from '@arcgis/core/widgets/Search'
 export const createSearchWidget = (view: __esri.MapView | __esri.SceneView) => {
   const searchWidgetDiv = document.createElement('div')
   searchWidgetDiv.id = 'searchWidgetDiv'
-  searchWidgetDiv.className = 'absolute top-4 left-2 p-2 z-10'
+  searchWidgetDiv.className = 'absolute top-20 left-2 p-2 z-10'
   view.ui.add(searchWidgetDiv)
 
   const searchWidget = new Search({
