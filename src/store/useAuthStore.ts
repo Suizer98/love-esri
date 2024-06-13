25f7edb80ca4df6ecac46066a8100d2ba287e42c
Sui Zer <suizer@nus.edu.sg>
2024-06-13 16:57:52 +0800

    Use default user if not logged in now, for easier demo purpose

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,3 +1,4 @@
+import esriConfig from '@arcgis/core/config'
 import IdentityManager from '@arcgis/core/identity/IdentityManager'
 import OAuthInfo from '@arcgis/core/identity/OAuthInfo'
 import Portal from '@arcgis/core/portal/Portal'
@@ -15,6 +16,7 @@ export const useAuthStore = create<AuthState>((set) => ({
 
   checkExistingSession: async (): Promise<string> => {
     const clientId = import.meta.env.VITE_CLIENT_ID
+    const apiKey = import.meta.env.VITE_ESRI_API
 
     const info = new OAuthInfo({
       appId: clientId,
@@ -26,21 +28,34 @@ export const useAuthStore = create<AuthState>((set) => ({
 
     try {
       await IdentityManager.checkSignInStatus(`${info.portalUrl}/sharing`)
-      const portal = new Portal()
-      await portal.load()
-      const user = portal.user
-      const userInfo = {
-        username: user.username,
-        fullName: user.fullName,
-        email: user.email,
-        role: user.role
-      }
-      set({ user: userInfo })
-      localStorage.setItem('user', JSON.stringify(userInfo))
+      // const portal = new Portal()
+      // await portal.load()
+      // const user = portal.user
+      // const userInfo = {
+      //   username: user.username,
+      //   fullName: user.fullName,
+      //   email: user.email,
+      //   role: user.role
+      // }
+      // set({ user: userInfo })
+      // localStorage.setItem('user', JSON.stringify(userInfo))
       return 'success'
     } catch (error) {
-      set({ user: null })
-      return 'error'
+      // If the user is not signed in, use the API key
+      if (apiKey) {
+        esriConfig.apiKey = apiKey
+
+        const userInfo = {
+          username: 'Default User',
+          role: 'Default'
+        }
+        set({ user: userInfo })
+        localStorage.setItem('user', JSON.stringify({ apiKey }))
+        return 'no_sign_in_but_api_key'
+      } else {
+        set({ user: null })
+        return 'error'
+      }
     }
   },
 
