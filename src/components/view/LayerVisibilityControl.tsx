0c97b9da042315a2336f6a27547a5349d5adc4b5
Sui Zer <suizer@nus.edu.sg>
2024-06-14 15:50:04 +0800

    Revert "Simplified layer control file"
    This reverts commit 9fae440cbe0fae44aceed996b262866b82c8e392.

--- src/components/view/LayerVisibilityControl.tsx
+++ src/components/view/LayerVisibilityControl.tsx
@@ -11,31 +11,58 @@ import { useViewStore } from '../../store/useViewStore'
 const LayerVisibilityControl: React.FC = () => {
   const { layers, toggleLayerVisibility } = useLayersStore()
   const { viewRef, mapType, isMapAvailable } = useMapStore()
-  const { isDesktopMode, toggleSidebar } = useViewStore()
+  const { isDesktopMode, toggleSidebar } = useViewStore((state) => ({
+    isDesktopMode: state.isDesktopMode,
+    toggleSidebar: state.toggleSidebar
+  }))
 
-  const updateCameraPosition = async (layerName: string, position3D: boolean) => {
-    if (viewRef) {
-      const view = viewRef as SceneView | MapView
+  const updateCameraPosition3D = async (layerName: string) => {
+    if (viewRef && mapType === '3D') {
+      const view = viewRef as SceneView
+      const layer = view.map.findLayerById(layerName)
+      if (layer) {
+        await layer.when()
+        const extent = layer.fullExtent
+        if (extent) {
+          if (isDesktopMode) {
+            view.goTo({
+              target: extent,
+              position: {
+                longitude: extent.center.x,
+                latitude: extent.center.y,
+                z: 314.88439
+              },
+              heading: 356.82,
+              tilt: 78.61
+            })
+          } else {
+            view.goTo({
+              position: {
+                longitude: -122.39899666,
+                latitude: 37.77940678,
+                z: 314.88439
+              },
+              heading: 356.82,
+              tilt: 78.61
+            })
+          }
+        }
+      }
+    }
+  }
+
+  const updateCameraPosition2D = async (layerName: string) => {
+    if (viewRef && mapType === '2D') {
+      const view = viewRef as MapView
       const layer = view.map.findLayerById(layerName) as ImageryTileLayer
       if (layer) {
         await layer.when()
         const extent = layer.fullExtent
         if (extent) {
-          const options = position3D
-            ? {
-                target: extent,
-                position: {
-                  longitude: extent.center.x,
-                  latitude: extent.center.y,
-                  z: 314.88439
-                },
-                heading: 356.82,
-                tilt: 78.61
-              }
-            : extent
-
-          view.goTo(options)
-          if (!position3D && (view as MapView).zoom < 3) (view as MapView).zoom = 3
+          await view.goTo(extent)
+          if (view.zoom < 3) {
+            view.zoom = 3
+          }
         }
       }
     }
@@ -45,28 +72,32 @@ const LayerVisibilityControl: React.FC = () => {
     const layer = layers.find((l) => l.name === layerName)
     if (layer) {
       toggleLayerVisibility(layerName)
-      if (!layer.visible) {
-        if (layerName === '3D Buildings' && mapType === '3D') updateCameraPosition(layerName, true)
-        else if (layerName === '2D Flow' && mapType === '2D') updateCameraPosition(layerName, false)
-        else if (layerName === 'Satellites') {
-          const recenterButton = document.querySelector(
-            '.recenter-button.esri-widget--button.esri-widget'
-          )
-          recenterButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+      if (layerName === '3D Buildings' && viewRef && !layer.visible) {
+        updateCameraPosition3D(layerName)
+      } else if (layerName === '2D Flow' && viewRef && !layer.visible) {
+        updateCameraPosition2D(layerName)
+      } else if (layerName === 'Satellites' && viewRef) {
+        const recenterButton = document.querySelector(
+          '.recenter-button.esri-widget--button.esri-widget'
+        )
+        if (recenterButton) {
+          recenterButton.dispatchEvent(new MouseEvent('click', { bubbles: true }))
         }
       }
-      if (!isDesktopMode) toggleSidebar()
+      if (!isDesktopMode) {
+        toggleSidebar()
+      }
     }
   }
 
+  // Ensure 3D Buildings and 2D Flow layers are unchecked and disabled in 2D and 3D modes respectively
   useEffect(() => {
-    ;['3D Buildings', '2D Flow'].forEach((layerName) => {
+    const layersToToggle = ['3D Buildings', '2D Flow']
+    layersToToggle.forEach((layerName) => {
       const layer = layers.find((l) => l.name === layerName)
       if (
-        layer &&
-        layer.visible &&
-        ((mapType === '2D' && layerName === '3D Buildings') ||
-          (mapType === '3D' && layerName === '2D Flow'))
+        (mapType === '2D' && layerName === '3D Buildings' && layer && layer.visible) ||
+        (mapType === '3D' && layerName === '2D Flow' && layer && layer.visible)
       ) {
         toggleLayerVisibility(layer.name)
       }
@@ -80,9 +111,15 @@ const LayerVisibilityControl: React.FC = () => {
           <Checkbox
             key={layer.name}
             isChecked={
-              layer.type === mapType || layer.name === 'Satellites' ? layer.visible : false
+              (layer.type === '3D' && mapType === '2D') || (layer.type === '2D' && mapType === '3D')
+                ? false
+                : layer.visible
             }
-            isDisabled={(layer.type !== mapType && layer.name !== 'Satellites') || !isMapAvailable}
+            isDisabled={
+              (layer.type === '3D' && mapType === '2D') ||
+              (layer.type === '2D' && mapType === '3D') ||
+              !isMapAvailable
+            }
             onChange={() => handleLayerToggle(layer.name)}
           >
             {layer.name}
